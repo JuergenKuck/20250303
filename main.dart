5b37bec3b062cf9e78a03111de80b6e505ab6e4d
JuergenKuck <drj.kuck@googlemail.com>
2025-03-03 12:59:37 +0100

    fertig

--- main.dart
+++ main.dart
@@ -1,3 +1,17 @@
 void main() {
-  print("Hallo");
+  print(getMovieRating(15, true, 16));
+  print(getMovieRating(13, false, 16));
+}
+
+String getMovieRating(int age, bool hasParentalConsent, int movingAgeRating) {
+  String result =
+      "Alter: $age, Elternbegleitung: $hasParentalConsent, Altersfreigabe: $movingAgeRating";
+
+  if (age >= movingAgeRating ||
+      movingAgeRating - age <= 2 && hasParentalConsent) {
+    result = "$result -> Film erlaubt!";
+  } else {
+    result = "$result -> Film nicht erlaubt!";
+  }
+  return result;
 }
